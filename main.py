d13b9505ab6e515a385cd60a4454c561956532de
ealtuntas <72756072+ealtuntas@users.noreply.github.com>
2021-10-01 10:04:26 +0000

    done

---
+++ main.py
@@ -0,0 +1,11 @@
+x = int(input("Enter number x:"))
+y = int(input("Enter number y:"))
+print(x**y)
+
+import math
+#z= input(numpy.log2(x))
+#print("71483")
+x = int(input("enter another number:"))
+z=int(math.log2(x))
+print(z)
+print("71483")
